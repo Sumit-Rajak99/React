f5d783ddba62914e8e9c2d97cb93c2250c797323
Sumit-Rajak99 <sumitrajaksumitrajak793@gmail.com>
2026-02-06 21:47:22 +0530

    compare data sign up with log in

--- localstorage/src/Login.jsx
+++ localstorage/src/Login.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react"
+import { Navigate, useNavigate } from "react-router-dom"
+
 function Login(){
+    let [loginfrm,loginsetfrm]=useState({
+        email:"",password:""
+    })
+    let navigator=useNavigate()
+
+    function logindata(e){
+    const {name,value}=e.target 
+    loginsetfrm({...loginfrm,[name]:value})
+
+    }
+  function loginhandle(e){
+    e.preventDefault()
+    let localdata=JSON.parse(localStorage.getItem("userdata"))
+    if(loginfrm.email!=localdata.email || loginfrm.password!=localdata.password){
+        alert("user not found")
+    }
+    else{
+        alert("welcome")
+    }
+  }
     return(
         <>
         <h1>this is login page</h1>
+        <form onSubmit={loginhandle}>
+            <label htmlFor="">email</label>
+            <input type="email" name="email" onChange={logindata} /><br /><br />
+            <label htmlFor="">password</label>
+            <input type="text" name="password" onChange={logindata} /> <br /><br />
+            <input type="submit" />
+        </form>
         </>
     )
 }
